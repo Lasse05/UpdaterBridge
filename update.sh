c684a1d7fd4093de5dbb5e2a1fbe621ae8f88553
Lasse05 <34139261+Lasse05@users.noreply.github.com>
2021-02-11 12:58:31 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 sudo rm -r /home/pi/tmp/
 sudo mkdir /home/pi/tmp/
 sudo chmod 7777 -R /home/pi/tmp/
-git clone https://7ce2928386b2f071ac66e8d6de5bc9407a7f6dd6@github.com/Lasse05/BetaScanner.git /home/pi/tmp
+git clone https://github.com/Lasse05/BetaScanner.git /home/pi/tmp
 sudo service beta stop
 sudo rm -r /home/pi/scanner
 sudo mv  /home/pi/tmp/ /home/pi/scanner/
