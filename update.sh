0d0ee0e6fa3d6180a244df7c1ea0f262532231f2
Lasse05 <34139261+Lasse05@users.noreply.github.com>
2021-02-21 13:33:30 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 sudo rm -r /home/pi/tmp/
 sudo chmod 7777 -R /home/pi/tmp/
 wget http://betascanner.s3.amazonaws.com/tmp.zip
-unzip tmp.zip
+unzip tmp.zip -d tmp/
 sudo service beta stop
 sudo rm -r /home/pi/scanner
 sudo mv  /home/pi/tmp/ /home/pi/scanner/
