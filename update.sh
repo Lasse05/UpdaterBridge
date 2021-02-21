e2d4a300978b6a9ed952adefdfd40f8dc3abaf31
Lasse05 <34139261+Lasse05@users.noreply.github.com>
2021-02-21 15:19:43 +0100

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo rm -r /home/pi/tmp/
 sudo chmod 7777 -R /home/pi/tmp/
-wget http://betascanner.s3.amazonaws.com/tmp.zip
-unzip tmp.zip -d tmp/
+sudo rm -r Scanner.zip*
+sudo apt-get install unzip
+wget http://betascanner.s3.amazonaws.com/Scanner.zip
+unzip Scanner.zip -d /home/pi/tmp/
 sudo service beta stop
 sudo rm -r /home/pi/scanner
 sudo mv  /home/pi/tmp/ /home/pi/scanner/
